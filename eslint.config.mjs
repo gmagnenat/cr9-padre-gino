dad8a0672b419276d3f7afb3835b7449ab32aeb8
G. Mag <g.magnenat@gmail.com>
2025-11-23 19:48:58 +0100

    feat: add tanstack query and prepare past orders route

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import prettier from 'eslint-config-prettier';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -18,6 +19,7 @@ export default [
   },
   reactHooks.configs.flat.recommended,
   reactPlugin.configs.flat['jsx-runtime'],
+  ...pluginQuery.configs['flat/recommended'],
   {
     files: ['**/*.js', '**/*.jsx'],
     plugins: {
@@ -35,6 +37,10 @@ export default [
       'react/no-unescaped-entities': 'off',
       'react/prop-types': 'off',
       'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/anchor-is-valid': 'error',
+      'jsx-a11y/anchor-has-content': 'error',
+      'jsx-a11y/anchor-ambiguous-text': 'error',
+      'jsx-a11y/img-redundant-alt': 'error',
     },
   },
   prettier,
